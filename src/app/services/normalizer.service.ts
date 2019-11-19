9b9159667aa402eb88825403f57d8de83ac7f95b
Marcos Colina <mcolina@beesion.com>
2019-11-19 19:39:23 -0300

    implement normalizer service

--- src/app/services/normalizer.service.ts
+++ src/app/services/normalizer.service.ts
@@ -4,20 +4,88 @@ import { Injectable } from '@angular/core';
 
 export class NormalizerService {
 
-  normalize(file:any) {
-    let fileReader = new FileReader();
+  //characters for array split
+  private profile: any = {
+    rowSeparator: '\n',
+    recordSeparator: '/',
+    recordDataSeparator: ' '
+  };
 
-    return new Promise(function (resolve, reject) {
-      fileReader.onerror = function() {
-        reject("Error");
-      };
+  //main function
+  normalize(fileContent:string) {
+    //get rows from file content
+    return this.getRows(fileContent)
+                //apply row join policy in new array
+                .reduce(this.reduceRows.bind(this), []);
+  }
 
-      fileReader.onloadend = function() {
-        resolve(fileReader.result);
-      };
+  private getRows(fileContent) {
+    //clean spaces from file content, then split it by its separator in profile
+    return fileContent.trim().split(this.profile.rowSeparator);
+  }
 
-      fileReader.readAsText(file);
-    });
+  private reduceRows(acc, rowContent) {
+    //get records from row content
+    let recs = this.getRecords(rowContent)
+                    //apply record join policy in new array
+                    .reduce(this.reduceRecords.bind(this), [])
+                    //filter looking for unique records
+                    .filter(function (rec) {
+                      return !this.some(function (item) {
+                        //validate equality in depth, code and description
+                        return item.depth == this.depth &&
+                                item.code == this.code &&
+                                item.description == this.description;
+                      }, rec);
+                    }, acc);
+
+    //join new records with accumulated ones
+    return acc.concat(recs);
+  }
+
+  private getRecords(rowContent) {
+    //clean spaces from row content, then split it by its separator in profile
+    return rowContent.trim().split(this.profile.recordSeparator);
+  }
+
+  private reduceRecords(acc, recordContent, idx) {
+    //destructure the code and description from record data
+    let [
+      code = '',
+      description = ''
+
+    ] = this.getRecordData(recordContent);
+
+    //destructure the code and description (parental info) from previous record data
+    //if root case, the value will be '-'
+    let {
+      code: parentCode = '-',
+      description: parentDescription = '-'
+
+    } = acc[idx-1] || {};
+
+    //validate the code and description have value
+    if (code != '' || description != '') {
+
+      //add record data in preliminar array
+      acc.push({
+        depth: idx+1,
+        code: code,
+        description: description,
+        parentCode: parentCode,
+        parentDescription: parentDescription
+      });
+
+    }
+
+    return acc;
+  }
+
+  private getRecordData(recordContent) {
+    //clean spaces from record content, then split it by its separator in profile
+    let data = recordContent.trim().split(this.profile.recordDataSeparator);
+    //return a new array with 1st data (code) and the rest joined (description)
+    return [data.shift().trim(), data.join(this.profile.recordDataSeparator).trim()];
   }
 
 }
--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -9,27 +9,47 @@ import { NormalizerService } from '../services/normalizer.service';
 
 export class UploaderComponent {
 
-  files: any = [];
+  //files to normalize
+  private files: any = [];
 
-  constructor(private ns: NormalizerService) {
+  //current normalized content
+  private normalizedContent: any = [];
 
+  //file reader instance
+  private fileReader: FileReader = new FileReader();
+
+  //constructor, create private instance of the normalizer service and handler for the reading operation ended
+  constructor(private _normalizerService: NormalizerService) {
+
+    //handler for the reading operation ended
+    this.fileReader.onloadend = function() {
+      //get file reader result as string
+      let stringContent = this.fileReader.result as string;
+      //normalize the result using the normalizer service
+      this.normalizedContent = this._normalizerService.normalize(stringContent);
+      //sort the normalized value
+      this.normalizedContent = this.normalizedContent.sort((a,b)=>a.depth>b.depth?1:-1);
+    }.bind(this);
   }
 
+  //called on drop event or on input file change
   uploadFile(event) {
+    //iterate in case of more than one file included
     for (let index = 0; index < event.length; index++) {
       const element = event[index];
+
+      //add in list of files to normalize
       this.files.push(element)
     }  
   }
 
   normalizeAttachment(index) {
-    let file = this.files[index];
-    this.ns.normalize(file).then(function (res) {
-      console.log(res);
-    })
+    //read file as plain text file
+    this.fileReader.readAsText(this.files[index]);
   }
 
   deleteAttachment(index) {
+    //remove item from list of files to normalize
     this.files.splice(index, 1)
   }
 
