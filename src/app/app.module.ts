03ccfbd25d8f2ca9bd51ee58cdf60173331cb0af
mjcrsis22 <mjcrsis22@gmail.com>
2019-11-20 09:49:53 -0300

    material include + textArea input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { AppComponent } from './app.component';
 import { NormalizerService } from './services/normalizer.service';
 import { DndDirective } from './directives/dnd.directive';
 import { UploaderComponent } from './uploader/uploader.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,7 +27,17 @@ import { UploaderComponent } from './uploader/uploader.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [NormalizerService],
   bootstrap: [AppComponent]
--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NormalizerService } from '../services/normalizer.service';
+import {Component, ViewChild} from '@angular/core';
+
+import {NormalizerService} from '../services/normalizer.service';
+
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-uploader',
@@ -9,27 +13,24 @@ import { NormalizerService } from '../services/normalizer.service';
 
 export class UploaderComponent {
 
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
   //files to normalize
   private files: any = [];
-
-  //current normalized content
-  private normalizedContent: any = [];
+  private txtArea: string = '';
 
   //file reader instance
   private fileReader: FileReader = new FileReader();
+
+  //current normalized content
+  private normalizedContentHeaders: string[] = [];
+  private normalizedContent = new MatTableDataSource();
 
   //constructor, create private instance of the normalizer service and handler for the reading operation ended
   constructor(private _normalizerService: NormalizerService) {
 
     //handler for the reading operation ended
-    this.fileReader.onloadend = function() {
-      //get file reader result as string
-      let stringContent = this.fileReader.result as string;
-      //normalize the result using the normalizer service
-      this.normalizedContent = this._normalizerService.normalize(stringContent);
-      //sort the normalized value
-      this.normalizedContent = this.normalizedContent.sort((a,b)=>a.depth>b.depth?1:-1);
-    }.bind(this);
+    this.fileReader.onloadend = this.readedFile.bind(this);
   }
 
   //called on drop event or on input file change
@@ -37,10 +38,19 @@ export class UploaderComponent {
     //iterate in case of more than one file included
     for (let index = 0; index < event.length; index++) {
       const element = event[index];
+      if (element.type == 'text/plain') {
+        //add in list of files to normalize
+        this.files.push(element)
+      } else {
+        console.log(`Cannot process files with type: "${element.type}"`);
+      }
+    }
+  }
 
-      //add in list of files to normalize
-      this.files.push(element)
-    }  
+  readedFile() {
+    //get file reader result as string
+    let stringContent = this.fileReader.result as string;
+    this.callNormalizer(stringContent);
   }
 
   normalizeAttachment(index) {
@@ -51,6 +61,28 @@ export class UploaderComponent {
   deleteAttachment(index) {
     //remove item from list of files to normalize
     this.files.splice(index, 1)
+  }
+
+  updateTxtArea(value) {
+    this.txtArea = value;
+  }
+
+  uploadTxtArea() {
+    this.callNormalizer(this.txtArea);
+  }
+
+  uploadClear() {
+    this.callNormalizer('');
+  }
+
+  callNormalizer(stringContent='') {
+    //normalize the result using the normalizer service
+    let result = this._normalizerService.normalize(stringContent);
+    this.normalizedContentHeaders = Object.keys(result[0] || {});
+    this.normalizedContent = new MatTableDataSource(result);
+
+    //sort the normalized value
+    this.normalizedContent.sort = this.sort;
   }
 
 }
